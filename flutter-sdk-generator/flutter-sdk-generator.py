9fae1032854fd098610404eebc3930c3fc423c85
Jan Koudijs <theappgineer@gmail.com>
2025-02-16 19:05:25 +0100

    Create workaround for Flutter 3.29.0 support

--- flutter-sdk-generator/flutter-sdk-generator.py
+++ flutter-sdk-generator/flutter-sdk-generator.py
@@ -205,6 +205,12 @@ def generate_sdk(
             'path': 'flutter-shared.sh.patch'
         },
         {
+            'type': 'shell',
+            'commands': [
+                'rm -f flutter/engine/src/.gn'
+            ]
+        },
+        {
             'type': 'script',
             'dest': 'flutter/bin',
             'dest-filename': 'setup-flutter.sh',
