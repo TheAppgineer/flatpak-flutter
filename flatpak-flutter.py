16a9caf2eccf2b98765b83e8ba6b70dc3665ceb4
Jan Koudijs <theappgineer@gmail.com>
2026-02-12 18:54:28 +0100

    Template: Update Flutter version to 3.41.0

--- flatpak-flutter.py
+++ flatpak-flutter.py
@@ -25,7 +25,7 @@ MODULES = 'generated/modules'
 SOURCES = 'generated/sources'
 PATCHES = 'generated/patches'
 
-TEMPLATE_FLUTTER_VERSION = '3.38.9'
+TEMPLATE_FLUTTER_VERSION = '3.41.0'
 DEFAULT_RUST_VERSION = '1.93.0'
 RUSTUP_PATH = '/var/lib/rustup'
 
